9f2fca14c778569ec0873f6d038bfc4c673c5d61
JeridiOmar <50057549+JeridiOmar@users.noreply.github.com>
2022-07-20 16:17:56 +0100

    init project

---
+++ src/main/java/com/example/multitenancydemo/MultiTenancyDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.multitenancydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiTenancyDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiTenancyDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/multitenancydemo/MultiTenancyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.multitenancydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiTenancyDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
